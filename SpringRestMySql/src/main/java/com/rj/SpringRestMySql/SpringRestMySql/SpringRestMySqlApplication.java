c1aaf8521a2b6e8e00893a39f7977166a7e73514
jaidka <ritika.jaidka1@gmail.com>
2019-01-13 22:58:50 -0500

    SpringBoot with MySQL

---
+++ SpringRestMySql/src/main/java/com/rj/SpringRestMySql/SpringRestMySql/SpringRestMySqlApplication.java
@@ -0,0 +1,14 @@
+package com.rj.SpringRestMySql.SpringRestMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMySqlApplication.class, args);
+	}
+
+}
+
---
+++ SpringRestMySql/src/test/java/com/rj/SpringRestMySql/SpringRestMySql/SpringRestMySqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rj.SpringRestMySql.SpringRestMySql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestMySqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
